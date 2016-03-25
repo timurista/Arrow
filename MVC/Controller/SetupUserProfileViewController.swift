14852f07f776c5aa8b389bad888bde88a951714e
Trevor Sharp <sharp.trevor@me.com>
2016-03-25 10:29:43 -0700

    Begin setup user profile

---
+++ MVC/Controller/SetupUserProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SetupUserProfileViewController.swift
+//  Arrow
+//
+//  Created by Trevor Sharp on 3/25/16.
+//  Copyright © 2016 Trevor Sharp. All rights reserved.
+//
+
+import UIKit
+
+class SetupUserProfileViewController: UIViewController {
+
+}
